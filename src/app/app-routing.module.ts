fbd368ccd1d8ebaeb46a0e2d4dec0f3bb3870628
yassir263 <yasschirawi11@gmail.com>
2022-05-04 20:35:51 +0200

    first project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { CentresComponent } from './centres/centres.component';
+import { CompetencesComponent } from './competences/competences.component';
+import { HomeComponent } from './home/home.component';
+import {ExperiencesComponent} from './experiences/experiences.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes =[
+{path : '', redirectTo : '/home',pathMatch:"full"},
+{path : 'aboutme', component : AboutMeComponent} ,
+{path : 'home',component : HomeComponent},
+{path:'centres',component:CentresComponent},{path: 'knowledges',component:CompetencesComponent},
+{path:'experiences',component:ExperiencesComponent},
+{path:'contact', component:ContactComponent},
+
+
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { CompetencesComponent } from './competences/competences.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
+import { CentresComponent } from './centres/centres.component';
+import { ContactComponent } from './contact/contact.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutMeComponent,
+    CompetencesComponent,
+    ExperiencesComponent,
+    CentresComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
